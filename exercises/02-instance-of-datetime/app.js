cb20449802b3028ebf9fbd0469812a7c82957b19
Alejandro Sanchez <aalejo@gmail.com>
2021-02-01 14:50:11 +0000

    switched to learnpack to avoid errors

---
+++ exercises/02-instance-of-datetime/app.js
@@ -0,0 +1,2 @@
+let d = new Date();
+console.log("Today is "+(d.getMonth()+1)+"-"+d.getDate()+"-"+d.getFullYear());
--- exercises/02-instance-of-datetime/test.js
+++ exercises/02-instance-of-datetime/test.js
@@ -8,6 +8,10 @@ it('You should use the console.log function to print the date', function () {
     expect(console.log.mock.calls.length).toBe(1);
 });
 
+test('Make sure to include the colon ":" after the "Today is"', function(){
+    expect(_buffer.includes(":")).toBe(true);
+})
+
 it("You should print on the console today's date with the exact format!", function () {
   let d = new Date(),
   month = '' + (d.getMonth() +1),
